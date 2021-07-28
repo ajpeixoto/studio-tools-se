421864cf35466d8917eaf0a437263f06bbe35b49
bhe-talendbj <bhe@talend.com>
2021-07-28 10:29:31 +0800

    fix(TUP-32347): fix java version parsing

--- studio-utils/src/main/java/org/talend/utils/JavaVersion.java
+++ studio-utils/src/main/java/org/talend/utils/JavaVersion.java
@@ -15,6 +15,8 @@ package org.talend.utils;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang3.StringUtils;
+
 /*
  * Created by bhe on Dec 24, 2019
  */
@@ -22,7 +24,7 @@ public class JavaVersion implements Comparable<JavaVersion> {
 
     private static final Logger LOGGER = Logger.getLogger(JavaVersion.class.getCanonicalName());
 
-    private int major, minor, security;
+    private int major, minor, buildNumber, security;
 
     public JavaVersion(String v) {
         parseVersion(v);
@@ -32,7 +34,10 @@ public class JavaVersion implements Comparable<JavaVersion> {
     public int compareTo(JavaVersion o) {
         if (this.major - o.major == 0) {
             if (this.minor - o.minor == 0) {
-                return this.security - o.security;
+                if (this.buildNumber - o.buildNumber == 0) {
+                    return this.security - o.security;
+                }
+                return this.buildNumber - o.buildNumber;
             }
             return this.minor - o.minor;
         }
@@ -40,7 +45,7 @@ public class JavaVersion implements Comparable<JavaVersion> {
     }
     
     private String normalizeVersion(String v) {
-        v = v.replaceAll("[^\\d.]", "");
+        v = v.replaceAll("[^\\d._]", "");
         if (v.isEmpty()) {
             v = "0";
         }
@@ -51,27 +56,34 @@ public class JavaVersion implements Comparable<JavaVersion> {
         if (v == null || v.isEmpty()) {
             return;
         }
-        String[] version = v.split("[\\._]");
-        String majorV = version[0];
-        majorV = normalizeVersion(majorV);
 
-        String minorV = version[1];
-        minorV = normalizeVersion(minorV);
+        String[] version = v.split("[\\._]");
+
         try {
-            this.major = Integer.parseInt(majorV);
+            this.major = Integer.parseInt(normalizeVersion(version[0]));
         } catch (Exception e) {
             LOGGER.log(Level.WARNING, "Major version parse error of " + v, e);
         }
-        if (version.length > 1) {
+
+        if (version.length > 1 && !StringUtils.isEmpty(version[1])) {
             try {
-                this.minor = Integer.parseInt(minorV);
+                this.minor = Integer.parseInt(normalizeVersion(version[1]));
             } catch (Exception e) {
                 LOGGER.log(Level.WARNING, "Minor version parse error of " + v, e);
             }
         }
-        if (version.length > 2) {
+
+        if (version.length > 2 && !StringUtils.isEmpty(version[2])) {
+            try {
+                this.buildNumber = Integer.parseInt(normalizeVersion(version[2]));
+            } catch (Exception e) {
+                LOGGER.log(Level.WARNING, "Build number parse error of " + v, e);
+            }
+        }
+
+        if (version.length > 3 && !StringUtils.isEmpty(version[3])) {
             // strip non number part if any
-            String securityNumber = version[version.length - 1];
+            String securityNumber = version[3];
             for (int i = 0; i < securityNumber.length(); i++) {
                 char c = securityNumber.charAt(i);
                 if (c > '9' || c < '0') {
@@ -92,6 +104,7 @@ public class JavaVersion implements Comparable<JavaVersion> {
         int result = 1;
         result = result * prime + this.major;
         result = result * prime + this.minor;
+        result = result * prime + this.buildNumber;
         result = result * prime + this.security;
         return result;
     }
@@ -113,10 +126,11 @@ public class JavaVersion implements Comparable<JavaVersion> {
         sb.append(".");
         sb.append(this.minor);
         sb.append(".");
-        if (this.major == 1) {
-            sb.append("0_");
+        sb.append(this.buildNumber);
+        if (this.security > 0) {
+            sb.append("_");
+            sb.append(this.security);
         }
-        sb.append(this.security);
         return sb.toString();
     }
 }
